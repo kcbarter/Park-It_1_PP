d2da5c45e4fd4288e77114bce9499d2d10ade003
Tyler Roemer <troemer@mail.greenriver.edu>
2018-05-03 14:43:33 -0400

    Added js functions

--- js/park-it.js
+++ js/park-it.js
@@ -1,3 +1,30 @@
+var storage;
+
+function init() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+    storage = window.localStorage;
+}
+
+function onDeviceReady() {
+    var node = document.createElement('link');
+    node.setAttribute('rel', 'stylesheet');
+    node.setAttribute('type', 'text/css');
+
+    if (cordova.platformid == 'ios'){
+        node.setAttribute('href', 'styles/park-it-ios.css');
+
+        window.StatusBar.overlaysWebView(false);
+        window.StatusBar.styleDefault();
+    }
+    else{
+        node.setAttribute('href', 'styles/park-it-android.css');
+        window.StatusBar.backgroundColorByHexString("#1565C0");
+    }
+
+    $('head').appendChild(node);
+}
+
 function initMap() {
     var grc = {lat: 47.313582, lng: -122.1800072};
     var hawaii = {lat: 21.9811, lng: -159.3711};
@@ -15,6 +42,11 @@ $(document).on("click", "#park", function () {
     alert("Set parking location");
 });
 
-$(document).on("click", "#find", function () {
+$(document).on("click", "#retrieve", function () {
     alert("Get parking location");
 });
+
+$(document).on('click', '#gotIt', function () {
+    $('#instructions').css('display', 'none');
+});
+
